fa75723c4a5356ac2a6364661b3c4369c4f1a185
ZOI\graham.geary <gg@zoi.de>
2019-11-12 21:16:06 +0100

    removed public subnets

--- vpc_transit_gateway_attachment.tf
+++ vpc_transit_gateway_attachment.tf
@@ -1,6 +1,6 @@
 resource "aws_ec2_transit_gateway_vpc_attachment" "network_transit_gateway" {
   count              = local.create_tgw_attachment
-  subnet_ids         = [aws_subnet.sn_private_a.id, aws_subnet.sn_private_b.id, aws_subnet.sn_private_c.id, aws_subnet.sn_public_a.id, aws_subnet.sn_public_b.id, aws_subnet.sn_public_c.id]
+  subnet_ids         = [aws_subnet.sn_private_a.id, aws_subnet.sn_private_b.id, aws_subnet.sn_private_c.id]
   transit_gateway_id = var.transit_gateway_id
   vpc_id             = aws_vpc.main.id
 
