846866ef0c32a72256de5102ce9938f484cc60e2
Emerson Navarro <emerson.navarro@zoi.de>
2020-04-06 17:45:44 +0200

    removed unnecessary double validation for subnets

--- vpc_subnets.tf
+++ vpc_subnets.tf
@@ -1,8 +1,8 @@
 # Private Subnets #
 # PRIVATE A
 resource "aws_subnet" "sn_private_a" {
-  # count = local.sn_private_a  
-  count = (local.enable_dynamic_subnets == true ? local.sn_private_a : 1)
+  count = local.sn_private_a  
+  # count = (local.enable_dynamic_subnets == true ? local.sn_private_a : 1)
   vpc_id = aws_vpc.main.id
   # cidr_block = (var.vpc_network == "" ? cidrsubnet(var.vpc_network, 3, 0) : var.private_subnets_a[count.index])
   cidr_block = (local.enable_dynamic_subnets == true ? var.private_subnets_a[count.index] : cidrsubnet(var.vpc_network,3,0))
@@ -16,8 +16,8 @@ resource "aws_subnet" "sn_private_a" {
 
 # PRIVATE B
 resource "aws_subnet" "sn_private_b" {
-  # count = local.sn_private_b
-  count = (local.enable_dynamic_subnets == true ? local.sn_private_b : 1)
+  count = local.sn_private_b
+  # count = (local.enable_dynamic_subnets == true ? local.sn_private_b : 1)
   vpc_id = aws_vpc.main.id
   # cidr_block = (var.vpc_network == "" ? cidrsubnet(var.vpc_network, 3, 1) : var.private_subnets_b[count.index])
   cidr_block = (local.enable_dynamic_subnets == true ? var.private_subnets_b[count.index] : cidrsubnet(var.vpc_network,3,1))
@@ -30,8 +30,8 @@ resource "aws_subnet" "sn_private_b" {
 
 # PRIVATE C
 resource "aws_subnet" "sn_private_c" {
-  # count = local.sn_private_c
-  count = (local.enable_dynamic_subnets == true ? local.sn_private_c : 1)
+  count = local.sn_private_c
+  # count = (local.enable_dynamic_subnets == true ? local.sn_private_c : 1)
   vpc_id = aws_vpc.main.id
   # cidr_block = (var.vpc_network == "" ? cidrsubnet(var.vpc_network, 3, 2) : var.private_subnets_c[count.index])
   cidr_block = (local.enable_dynamic_subnets == true ? var.private_subnets_c[count.index] : cidrsubnet(var.vpc_network,3,2))
@@ -45,8 +45,8 @@ resource "aws_subnet" "sn_private_c" {
 # Public Subnets #
 # PUBLIC A
 resource "aws_subnet" "sn_public_a" {
-  # count = local.sn_public_a
-  count = (local.enable_dynamic_subnets == true ? local.sn_public_a : 1)
+  count = local.sn_public_a
+  # count = (local.enable_dynamic_subnets == true ? local.sn_public_a : 1)
   vpc_id = aws_vpc.main.id
   # cidr_block        = (var.vpc_network == "" ? cidrsubnet(var.vpc_network, 3, 4) : var.public_subnets_a[count.index]) # cidrsubnet(var.vpc_network, 3, 4)
   cidr_block = (local.enable_dynamic_subnets == true ? var.public_subnets_a[count.index] : cidrsubnet(var.vpc_network,3,4))
@@ -60,8 +60,8 @@ resource "aws_subnet" "sn_public_a" {
 
 # PUBLIC B
 resource "aws_subnet" "sn_public_b" {
-  # count = local.sn_public_b
-  count = (local.enable_dynamic_subnets == true ? local.sn_public_b : 1)
+  count = local.sn_public_b
+  # count = (local.enable_dynamic_subnets == true ? local.sn_public_b : 1)
   vpc_id            = aws_vpc.main.id
   # cidr_block        = (var.vpc_network == "" ? cidrsubnet(var.vpc_network, 3, 5) : var.public_subnets_b[count.index])# cidrsubnet(var.vpc_network, 3, 4)
   cidr_block = (local.enable_dynamic_subnets == true ? var.public_subnets_b[count.index] : cidrsubnet(var.vpc_network, 3, 5))
@@ -75,8 +75,8 @@ resource "aws_subnet" "sn_public_b" {
 
 # PUBLIC C
 resource "aws_subnet" "sn_public_c" {
-  # count = local.sn_public_c
-  count = (local.enable_dynamic_subnets == true ? local.sn_public_c : 1)
+  count = local.sn_public_c
+  # count = (local.enable_dynamic_subnets == true ? local.sn_public_c : 1)
   vpc_id            = aws_vpc.main.id
   # cidr_block        = (var.vpc_network == "" ? cidrsubnet(var.vpc_network, 3, 6) : var.public_subnets_c[count.index])# cidrsubnet(var.vpc_network, 3, 5)
   cidr_block = (local.enable_dynamic_subnets == true ? var.public_subnets_c[count.index] : cidrsubnet(var.vpc_network, 3, 6))
