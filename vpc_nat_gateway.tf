19eb8477d2624f0e7f3d9c7f23d2c74916fbb1ee
ZOI\oliver.annau <22314584+oliver1980@users.noreply.github.com>
2020-09-07 15:29:24 +0200

    cosmetic changes after running terraform fmt

--- vpc_nat_gateway.tf
+++ vpc_nat_gateway.tf
@@ -4,10 +4,10 @@ resource "aws_eip" "natgw_eip" {
 }
 
 resource "aws_nat_gateway" "natgw" {
-  count         = local.create_nat 
+  count         = local.create_nat
   allocation_id = aws_eip.natgw_eip.0.id
-  subnet_id = local.vpc_nat_gateway_subnet_id
-  depends_on = [aws_internet_gateway.igw]
+  subnet_id     = local.vpc_nat_gateway_subnet_id
+  depends_on    = [aws_internet_gateway.igw]
 
-  tags =  local.nat_gw_tags 
+  tags = local.nat_gw_tags
 }
