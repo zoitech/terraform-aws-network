440550bacfaa5dae2218a1fe19bb33789bfc2379
ZOI\oliver.annau <22314584+oliver1980@users.noreply.github.com>
2020-09-07 15:29:52 +0200

    adding less permissive policies than default

--- vpc_endpoints.tf
+++ vpc_endpoints.tf
@@ -1,10 +1,26 @@
+# Endpoints
+
 resource "aws_vpc_endpoint" "s3" {
   count  = local.create_vpcep_s3
   vpc_id = aws_vpc.main.id
 
   service_name = "com.amazonaws.${var.region}.s3"
-
-  tags = local.vpcep_s3_tags
+  policy       = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Id": "Policy1568307208199",
+    "Statement": [
+        {
+            "Sid": "Stmt1568307206805",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:*",
+            "Resource": "*"
+        }
+    ]
+}
+POLICY
+  tags         = local.vpcep_s3_tags
 }
 
 resource "aws_vpc_endpoint" "dynamodb" {
@@ -12,6 +28,20 @@ resource "aws_vpc_endpoint" "dynamodb" {
   vpc_id = aws_vpc.main.id
 
   service_name = "com.amazonaws.${var.region}.s3"
-
-  tags = local.vpcep_dynamodb_tags
+  policy       = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Id": "Policy1568307208199",
+    "Statement": [
+        {
+            "Sid": "Stmt1568307206805",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "dynamodb:*",
+            "Resource": "*"
+        }
+    ]
+}
+POLICY
+  tags         = local.vpcep_dynamodb_tags
 }
