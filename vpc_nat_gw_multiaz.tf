025b94a590681e5f2a4840160667e9dcb2b19e66
Guillermo Contreras <guillermo.contreras@zoi.tech>
2024-07-25 12:39:08 -0600

    For expression to handle s3 route

--- vpc_nat_gw_multiaz.tf
+++ vpc_nat_gw_multiaz.tf
@@ -68,10 +68,10 @@ resource "aws_route" "rt_private_multiaz_tgw_route_3" {
 }
 
 resource "aws_vpc_endpoint_route_table_association" "rt_private_multiaz_s3_endpoint" {
-  for_each = var.create_vpcep_s3 ? aws_route_table.rt_private_multiaz : []
+  for_each = { for rt, values in aws_route_table.rt_private_multiaz : rt => values if var.create_vpcep_s3 }
 
   vpc_endpoint_id = aws_vpc_endpoint.s3[0].id
-  route_table_id  = aws_route_table.rt_private_multiaz[each.key].id
+  route_table_id  = aws_route_table.rt_private_multiaz[each.key].idcd 
 }
 
 resource "aws_route_table_association" "rt_private_a_multiaz" {
