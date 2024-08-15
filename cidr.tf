07359e2625ec62940c172389024486e294e5e408
Aleksandr Gayvoronskiy <aleksandr.gayvoronskiy@gmail.com>
2024-08-15 12:31:28 +0200

    Convert to map

--- cidr.tf
+++ cidr.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc_ipv4_cidr_block_association" "additional_cidr" {
-  for_each      = var.vpc_additional_cidr
+  for_each      = zipmap(var.vpc_additional_cidr, var.vpc_additional_cidr)
   vpc_id        = aws_vpc.main.id
   cidr_block    = each.value
 }
